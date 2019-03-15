aaed09d842d26d9838ae4e3c7a71b3efa8bcd809
uidq1829 <uidq1829@HZH4565C.v01.net>
2019-03-15 15:21:04 +0800

    优化数据库

--- src/Guoxin/SetupDatabase.sql
+++ src/Guoxin/SetupDatabase.sql
@@ -1,6 +1,6 @@
-# Host: localhost  (Version: 5.5.34)
-# Date: 2019-03-03 19:35:23
-# Generator: MySQL-Front 5.3  (Build 4.43)
+# Host: localhost  (Version: 5.5.53)
+# Date: 2019-03-15 15:17:45
+# Generator: MySQL-Front 5.3  (Build 4.234)
 
 /*!40101 SET NAMES utf8 */;
 
@@ -41,7 +41,7 @@ CREATE TABLE `expoxy_storage` (
 # Data for table "expoxy_storage"
 #
 
-INSERT INTO `expoxy_storage` VALUES ('ob789-20190212-l0012321','2019-02-28 21:16:28','10s04929',1),('ob789-20190212-s0012311','2019-02-28 21:10:19','10s04929',3);
+INSERT INTO `expoxy_storage` VALUES ('OB786-20181114-S0000061','2019-02-26 12:00:00','123',2),('ob786-20181114-s0000072','2019-03-05 18:02:45','10s04929',3);
 
 #
 # Structure for table "expoxy_unfreeze"
@@ -55,15 +55,15 @@ CREATE TABLE `expoxy_unfreeze` (
   PRIMARY KEY (`expoxy_num`),
   UNIQUE KEY `number` (`expoxy_num`) USING BTREE,
   KEY `staff1` (`unfreeze_operator`),
-  CONSTRAINT `staff1` FOREIGN KEY (`unfreeze_operator`) REFERENCES `staff` (`staff_num`),
-  CONSTRAINT `number` FOREIGN KEY (`expoxy_num`) REFERENCES `expoxy_storage` (`expoxy_num`) ON DELETE CASCADE ON UPDATE CASCADE
+  CONSTRAINT `number` FOREIGN KEY (`expoxy_num`) REFERENCES `expoxy_storage` (`expoxy_num`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `staff1` FOREIGN KEY (`unfreeze_operator`) REFERENCES `staff` (`staff_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
 # Data for table "expoxy_unfreeze"
 #
 
-INSERT INTO `expoxy_unfreeze` VALUES ('ob789-20190212-s0012311','2019-02-28 22:07:29','10s04929');
+INSERT INTO `expoxy_unfreeze` VALUES ('OB786-20181114-S0000061','2019-03-14 17:16:18','10s04929'),('ob786-20181114-s0000072','2019-03-05 16:02:54','10s04929');
 
 #
 # Structure for table "expoxy_use"
@@ -78,15 +78,15 @@ CREATE TABLE `expoxy_use` (
   PRIMARY KEY (`expoxy_num`),
   UNIQUE KEY `number` (`expoxy_num`) USING BTREE,
   KEY `staff2` (`user`),
-  CONSTRAINT `staff2` FOREIGN KEY (`user`) REFERENCES `staff` (`staff_num`),
-  CONSTRAINT `num` FOREIGN KEY (`expoxy_num`) REFERENCES `expoxy_unfreeze` (`expoxy_num`)
+  CONSTRAINT `num` FOREIGN KEY (`expoxy_num`) REFERENCES `expoxy_unfreeze` (`expoxy_num`),
+  CONSTRAINT `staff2` FOREIGN KEY (`user`) REFERENCES `staff` (`staff_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
 # Data for table "expoxy_use"
 #
 
-INSERT INTO `expoxy_use` VALUES ('ob789-20190212-s0012311','2019-03-03 18:58:52','10s04929','CA3');
+INSERT INTO `expoxy_use` VALUES ('ob786-20181114-s0000072','2019-03-05 18:14:13','10s04929','CA3');
 
 #
 # Structure for table "expoxy_callback"
@@ -97,11 +97,13 @@ CREATE TABLE `expoxy_callback` (
   `expoxy_num` varchar(255) NOT NULL DEFAULT '',
   `callbak_time` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   `callbacker` varchar(255) NOT NULL DEFAULT '0',
+  `callback_stauts` int(3) NOT NULL DEFAULT '1',
+  `callback_failreson` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`expoxy_num`),
   UNIQUE KEY `num` (`expoxy_num`),
   KEY `staff3` (`callbacker`),
-  CONSTRAINT `staff3` FOREIGN KEY (`callbacker`) REFERENCES `staff` (`staff_num`),
-  CONSTRAINT `nm` FOREIGN KEY (`expoxy_num`) REFERENCES `expoxy_storage` (`expoxy_num`)
+  CONSTRAINT `nm` FOREIGN KEY (`expoxy_num`) REFERENCES `expoxy_storage` (`expoxy_num`),
+  CONSTRAINT `staff3` FOREIGN KEY (`callbacker`) REFERENCES `staff` (`staff_num`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 #
